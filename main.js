efa2a18e11a7bb7f500735399914d900e7467b4e
RussellShire <russell.shire@gmail.com>
2022-07-26 20:44:30 +0100

    Starting to refactor into MVC architecture

--- main.js
+++ main.js
@@ -1,52 +1,63 @@
-const userInputBox = document.getElementById('user-input');
-const button = document.getElementById('button');
-let userInput = '';
-const consoleLogMode = false;
+const model = { userInput: '' }
 
-//QUESTION FOR GRAHAM: I've used userInput a lot as an arguement etc throughout different functions etc, is that bad practice?
+const view = {
+    consoleLogMode: false, //For switching on and off debugging
 
-const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper') {
-        consoleLogMode? console.log(`userInput within getUserChoice(): ${userInput}`) : '';
-        return userInput 
-    } else {
-      console.log('Please enter, rock, paper or scissors')
-    }
-  }
-  
-  const getComputerChoice = () => {
-    let choice = Math.floor(Math.random()*3);
-    const rockpaperscissors = ['rock', 'paper', 'scissors'];
-    return rockpaperscissors[choice]
+    selectRock: document.getElementById('rock-control'),
+    selectPaper: document.getElementById('paper-control'),
+    selectScissors: document.getElementById('scissors-control'),
     
-  }
-  
-  const determineWinner = (userChoice, computerChoice) => {
-    if (userChoice === computerChoice){
-      console.log('Draw!')
-    } else { 
-    if (userChoice === 'scissors'){
-      computerChoice === 'rock'? console.log('Computer wins') : console.log('You win!');
-    } if (userChoice === 'paper'){
-      computerChoice === 'scissors'? console.log('Computer wins') : console.log('You win!');
-    } if (userChoice === 'rock'){
-      computerChoice === 'paper'? console.log('Computer wins') : console.log('You win!');
-      }
+    //QUESTION FOR GRAHAM: Why can't I use this.selectRock below???
+    /*userSelection: document.getElementById('rock-control').addEventListener('click', () => { 
+       console.log('view rock select')}),*/
+       
+    render: (userInput) => {
+        
     }
-  }
-  
-  const playGame = (userInput) => {
-    const userChoice = getUserChoice(userInput);
-    const computerChoice = getComputerChoice();
-    console.log(`You chose ${userChoice}`)
-    console.log(`The computer chose ${computerChoice}`)
-    determineWinner(userChoice, computerChoice)
-  }
+    }
+
+const controller = {
+    getComputerChoice: () => {
+        let choice = Math.floor(Math.random()*3);
+        const rockpaperscissors = ['rock', 'paper', 'scissors'];
+        return rockpaperscissors[choice]},
+    determineWinner: (userChoice, computerChoice) => {
+        if (userChoice === computerChoice){
+          console.log('Draw!')
+        } else { 
+        if (userChoice === 'scissors'){
+          computerChoice === 'rock'? console.log('Computer wins') : console.log('You win!');
+        } if (userChoice === 'paper'){
+          computerChoice === 'scissors'? console.log('Computer wins') : console.log('You win!');
+        } if (userChoice === 'rock'){
+          computerChoice === 'paper'? console.log('Computer wins') : console.log('You win!');
+          }
+        }
+      },
+    playGame: (userInput) => {
+        const userChoice = userInput;
+        const computerChoice = controller.getComputerChoice();
+        console.log(`You chose ${userChoice}`)
+        console.log(`The computer chose ${computerChoice}`)
+        controller.determineWinner(userChoice, computerChoice)
+      },
+    
+}
+
+//QUESTION FOR GRAHAM: Event listeners should be in View right? I've been struggling to add them
 
   //event listener
-button.onclick = e => {
-    userInput = userInputBox.value
-    userInputBox.value = '';
-    playGame(userInput);
+view.selectRock.onclick = e => {
+    model.userInput = 'rock'
+    controller.playGame(model.userInput);
+}
+
+view.selectPaper.onclick = e => {
+    model.userInput = 'paper'
+    controller.playGame(model.userInput);
+}
+
+view.selectScissors.onclick = e => {
+    model.userInput = 'scissors'
+    controller.playGame(model.userInput);
 }
